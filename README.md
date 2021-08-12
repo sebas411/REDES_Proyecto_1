28dda7ab64baacce81507e54d6f9c7527e6e820e
sebas411 <sebasma411@gmail.com>
2021-08-12 16:58:52 -0600

    finish

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # REDES_Proyecto_1
+
+## Description
+
+This program was developed with the objective to learn about the xmpp protocol. It consists on 
+
+## Getting Started
+
+### Dependencies
+
+* Python 3
+* slixmpp module
+
+### Installing
+
+* First you need to clone or download the code of this repository to your computer.
+* Then install the slixmpp module by running 'pip install slimxpp'
+
+### Executing
+
+* To execute the program run the command 'python3 client.py'
+
+### Usage
+
+#### Available commands
+
+* quit  
+  closes the program
+
+* login [username] [password]  
+  logs into xmpp account
+
+* loggout  
+  logs you out of the account
+
+* update  
+  sends and receives notifications
+
+* message [user] [message]  
+  sends a message to specified user
+
+* presence [status] [show]  
+  sets custom status
+
+* adduser [user]  
+  adds a friend specified in [user]
+
+* userdetails [user]  
+  shows user details of specified user
+
+* showusers  
+  shows users and presence
+
+## Author
+
+* Sebastián Maldonado 18003
--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import asyncio
-import logging
 
 from slixmpp import ClientXMPP
 
@@ -8,53 +7,81 @@ class Client(ClientXMPP):
 
   def __init__(self, jid, password):
     ClientXMPP.__init__(self, jid, password)
+    self.id = jid
 
     self.add_event_handler("message", self.message)
     self.add_event_handler("session_start", self.session_start)
 
-    # If you wanted more functionality, here's how to register plugins:
-    # self.register_plugin('xep_0030') # Service Discovery
-    # self.register_plugin('xep_0199') # XMPP Ping
 
-    # Here's how to access plugins once you've registered them:
-    # self['xep_0030'].add_feature('echo_demo')
-
+  #all logic goes here
   async def session_start(self, event):
     self.send_presence()
-    self.get_roster()
+    await self.get_roster()
     while True:
-      commandline = input("xmppCli> ").split(' ')
+      commandline = input("xmppCli (%s)> "%self.id).split(' ')
       command = commandline[0]
       args = commandline[1:]
-      if command == "quit":
-        break
-      elif command == "login":
-        print("You are already logged in")
-      elif command == "message":
-        if len(args) < 2: continue
-        to = args[0]
-        msg = args[1]
-        self.send_message(to, msg, mtype='chat')
 
-    # Most get_*/set_* methods from plugins use Iq stanzas, which
-    # are sent asynchronously. You can almost always provide a
-    # callback that will be executed when the reply is received.
+      if command == "quit":
+        print("Please loggout first")
+        await self.get_roster()
+
+      elif command == "loggout":
+        self.disconnect()
+
+      elif command == "login": print("You are already logged in")
+
+      elif command == "message":
+        if len(args) >= 2:
+          to = args[0]
+          msg = ' '.join(args[1:])
+          self.send_message(to, msg, mtype='chat')
+        else:
+          print("command missing arguments")
+
+      elif command == "update": await self.get_roster()
+
+      elif command == "presence":
+        if len(args) >= 2:
+          status = args[0]
+          show = args[1]
+          self.send_presence(show, status)
+        else:
+          print("command missing arguments")
+
+      elif command == "adduser":
+        if len(args) == 1:
+          user = args[0]
+          self.send_presence_subscription(user)
+        else: print("incorrect amount of arguments")
+      
+      elif command == "userdetails":
+        if len(args) == 1:
+          user = args[0]
+          user = self.client_roster.presence(user)
+          for pres in user.items():
+            print("%(show)s %(status)s"%(pres, pres))
+        else: print("incorrect amount of arguments")
+      
+      elif command == "showusers":
+        contacts = self.client_roster.groups()
+        for contact in contacts:
+          print(contact)
+
+      else:
+        print("command not found")
+
+      await self.get_roster()
 
   def message(self, msg):
     if msg['type'] in ('chat', 'normal'):
-      print("Message: %(body)s"% msg)
-      #msg.reply("Thanks for sending\n%(body)s" % msg).send()
+      print("%(from)s says: %(body)s"% msg)
 
 async def session(user, psw):
   pass
 
 
 if __name__ == '__main__':
-  # Ideally use optparse or argparse to get JID,
-  # password, and log level.
-
-  #logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
-
   connected = False
   xmpp = 0
 
@@ -67,20 +94,18 @@ if __name__ == '__main__':
     elif command == "login":
       if connected: print("You are already logged in")
       else:
-        user = args[0]
-        psw = args[1]
-        xmpp = Client('echobot@alumchat.xyz', 'boty')
-        xmpp.connect()
-        xmpp.process(forever=False)
-        connected = True
-    elif command == "message":
-      if len(args) < 2: continue
-      to = args[0]
-      msg = args[1]
-      if connected:
-        xmpp.send_message(to, msg, mtype='chat')
+        if len(args) >= 2:
+          user = args[0]
+          psw = args[1]
+          xmpp = Client(user, psw)
+          xmpp.connect()
+          xmpp.process(forever=False)
+          connected = True
+        else:
+          print("command missing arguments")
+    else:
+      print("command not found or not logged in")
 
-    print(command, args)
       
 
   
